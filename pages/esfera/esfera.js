8bfddf74223aa4454938d96d4d58bd488fa5585a
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-05-04 21:26:23 -0300

    Concatenação e Interpolação adicionada

--- pages/esfera/esfera.js
+++ pages/esfera/esfera.js
@@ -6,6 +6,7 @@ let teste1 = areaEsfera(3)
 let teste2 = areaEsfera(15)
 let teste3 = areaEsfera(1523)
 
-console.log(teste1.toFixed(3))
-console.log(teste2.toFixed(3))
-console.log(teste3.toFixed(3))
+console.log(`VOLUME = ${teste1.toFixed(3)}`)
+console.log(`VOLUME = ${teste2.toFixed(3)}`)
+console.log(`VOLUME = ${teste3.toFixed(3)}`)
+
